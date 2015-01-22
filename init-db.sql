b67e01c61881ae39db8f725c3e601b27c9b70622
Julien Pellet <julien.pellet@ubisoft.com>
2015-01-22 15:36:40 -0500

    add pup CGI script and DB

--- init-db.sql
+++ init-db.sql
@@ -2,8 +2,7 @@ DROP database pup;
 CREATE database pup;
 USE pup;
 
-CREATE TABLE user (
+CREATE TABLE users (
   first_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
   last_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin
 );
-
---
+++ pup.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require 'cgi'
+require 'rubygems'
+require 'mysql2'
+
+cgi = CGI.new
+puts cgi.header
+puts "<html>
+<head>
+<style>
+html {
+    font-size: 40px;
+}
+table {
+    font-size: 40px;
+} 
+</style>
+</head>
+<body>"
+
+puts "This is a test"
+
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "pup")
+results = client.query("DESC users")
+
+puts "<table>"
+results.each do |row|
+  puts "<tr>"
+  puts "<td>#{row["Field"]}</td><td>#{row["Type"]}</td>" # row["id"].class == Fixnum
+  puts "</tr>"
+end
+puts "</table>"
+
+puts "<img src='http://resnlabs.com/wp-content/uploads/2012/05/GiddyUpright.gif'/>"
+
+puts "</body></html>"

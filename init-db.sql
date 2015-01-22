88eda062286cbc7eb4db5aa1b2abd061489af969
Julien Pellet <julien.pellet@ubisoft.com>
2015-01-22 14:45:09 -0500

    add init DB

---
+++ init-db.sql
@@ -0,0 +1,9 @@
+DROP database pup;
+CREATE database pup;
+USE pup;
+
+CREATE TABLE user (
+  first_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
+  last_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin
+);
+

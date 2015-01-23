3fb07d3fe513533ad99e2915c096fa5199040f7a
Julien Pellet <julien.pellet@ubisoft.com>
2015-01-23 09:16:17 -0500

    use DB in sql script

--- alter-db.sql
+++ alter-db.sql
@@ -1,3 +1,5 @@
+USE pup
+
 ALTER TABLE users ADD COLUMN lunch_and_learn BOOLEAN;
 
 UPDATE users SET url='http://reactiongifs.me/wp-content/uploads/2013/08/shia-labeouf-magic-gif.gif' WHERE first_name='lapin';

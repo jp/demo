88c5bb0cd531206f5bdc68d0e989f89f369e5e77
Julien Pellet <julien.pellet@ubisoft.com>
2015-01-23 08:50:58 -0500

    add record to db

--- init-db.sql
+++ init-db.sql
@@ -4,5 +4,8 @@ USE pup;
 
 CREATE TABLE users (
   first_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
-  last_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin
+  last_name CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin,
+  url CHAR(50) CHARACTER SET utf8 COLLATE utf8_bin
 );
+
+INSERT INTO users VALUES ('lapin','cretin','http://resnlabs.com/wp-content/uploads/2012/05/GiddyUpright.gif');
